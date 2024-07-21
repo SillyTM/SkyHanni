145ad8df792b990ae117d77c8db6b48d340442a8
Empa <42304516+ItsEmpa@users.noreply.github.com>
2024-07-21 16:19:24 +0200

    axisaligned
    Signed-off-by: Empa <42304516+ItsEmpa@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/features/event/carnival/CarnivalAPI.kt
+++ src/main/java/at/hannibal2/skyhanni/features/event/carnival/CarnivalAPI.kt
@@ -22,6 +22,7 @@ import at.hannibal2.skyhanni.utils.RenderUtils.drawString
 import at.hannibal2.skyhanni.utils.SimpleTimeMark
 import at.hannibal2.skyhanni.utils.StringUtils.removeColor
 import at.hannibal2.skyhanni.utils.repopatterns.RepoPattern
+import net.minecraft.util.AxisAlignedBB
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import kotlin.time.Duration.Companion.seconds
 
@@ -50,7 +51,10 @@ object CarnivalAPI {
     val currentTask: String?
         get() = ScoreboardPattern.carnivalTasksPattern.firstMatches(ScoreboardData.sidebarLinesFormatted)?.removeColor()
 
-    private val fruitBounds = LorenzVec(-112, 72, -11).axisAlignedTo(LorenzVec(-106, 72, -5))
+    private val fruitBounds = AxisAlignedBB(
+        -112.0, 72.0, -11.0,
+        -106.0, 72.0, -5.0,
+    )
 
     private val fruitBlocks = mutableMapOf<LorenzVec, Int>()
 
