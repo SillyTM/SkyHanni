a573d2b1ba8a79551596051ebbefd9771eabeec6
Empa <itsempa@users.noreply.github.com>
2024-12-13 21:53:49 +0100

    tenacity challenge cleanup and improvements
    Signed-off-by: Empa <itsempa@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/features/nether/dojo/TenacityChallenge.kt
+++ src/main/java/at/hannibal2/skyhanni/features/nether/dojo/TenacityChallenge.kt
@@ -1,23 +1,26 @@
 package at.hannibal2.skyhanni.features.nether.dojo
 
 import at.hannibal2.skyhanni.api.event.HandleEvent
+import at.hannibal2.skyhanni.data.IslandType
 import at.hannibal2.skyhanni.events.LorenzRenderWorldEvent
+import at.hannibal2.skyhanni.events.LorenzTickEvent
 import at.hannibal2.skyhanni.events.entity.EntityEnterWorldEvent
 import at.hannibal2.skyhanni.events.entity.EntityLeaveWorldEvent
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.utils.BlockUtils
-import at.hannibal2.skyhanni.utils.ItemUtils.name
 import at.hannibal2.skyhanni.utils.LorenzUtils
 import at.hannibal2.skyhanni.utils.LorenzVec
 import at.hannibal2.skyhanni.utils.NumberUtil.ordinal
 import at.hannibal2.skyhanni.utils.RenderUtils
 import at.hannibal2.skyhanni.utils.SoundUtils
 import at.hannibal2.skyhanni.utils.SoundUtils.playSound
+import at.hannibal2.skyhanni.utils.compat.getStandHelmet
+import at.hannibal2.skyhanni.utils.getLorenzVec
 import at.hannibal2.skyhanni.utils.getMotionLorenzVec
-import at.hannibal2.skyhanni.utils.toLorenzVec
 import net.minecraft.entity.Entity
 import net.minecraft.entity.item.EntityArmorStand
 import net.minecraft.entity.monster.EntityGhast
+import net.minecraft.init.Blocks
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import java.awt.Color
 import kotlin.time.Duration.Companion.seconds
@@ -27,28 +30,41 @@ object TenacityChallenge : DojoChallengeClass(DojoChallenge.TENACITY) {
 
     private val config get() = DojoAPI.config.tenacity
 
-    data class start_pos(val pos: LorenzVec)
-    private var projectileData: MutableMap<EntityArmorStand, start_pos> = mutableMapOf()
+    private data class ProjectileData(
+        val startPos: LorenzVec,
+        var endPos: LorenzVec? = null,
+    ) {
+        fun update(entity: EntityArmorStand) {
+            val motionVec = entity.getMotionLorenzVec()
+            endPos = BlockUtils.rayTrace(startPos, motionVec) ?: (startPos + motionVec)
+        }
+    }
+    private val projectileData = mutableMapOf<EntityArmorStand, ProjectileData>()
+
+    private val warningSound = SoundUtils.createSound("random.orb", 0.5f)
 
     private var ghastCount: Int = 0
 
-    @HandleEvent
+    @HandleEvent(onlyOnIsland = IslandType.CRIMSON_ISLE)
     fun onEntityEnterWorld(event: EntityEnterWorldEvent<Entity>) {
-        when (val entity = event.entity) {
+        if (!isActive()) return
+        val entity = event.entity
+        if (!DojoAPI.inDojoArena(entity.getLorenzVec())) return
+        when (entity) {
             is EntityGhast -> {
                 ghastCount++
                 warnGhastSpawn(ghastCount)
             }
             is EntityArmorStand -> {
-                val (_, stack) = entity.inventory.withIndex().firstOrNull { it.value != null } ?: return
-                if (stack.name != "Block of Coal") return
+                val stack = entity.getStandHelmet() ?: return
+                if (stack.item != Blocks.coal_block) return
 
-                projectileData += mapOf(entity to start_pos(entity.position.toLorenzVec()))
+                projectileData[entity] = ProjectileData(entity.getLorenzVec())
             }
         }
     }
 
-    @HandleEvent
+    @HandleEvent(onlyOnIsland = IslandType.CRIMSON_ISLE)
     fun onEntityLeaveWorld(event: EntityLeaveWorldEvent<EntityArmorStand>) {
         projectileData -= event.entity
     }
@@ -56,25 +72,39 @@ object TenacityChallenge : DojoChallengeClass(DojoChallenge.TENACITY) {
     private fun warnGhastSpawn(count: Int) {
         if (!config.ghastWarning) return
 
-        SoundUtils.createSound("random.orb", 0.5f).playSound()
+        warningSound.playSound()
         LorenzUtils.sendTitle("§c$count${count.ordinal()} Ghast is Spawning", duration = 3.seconds)
     }
 
     @SubscribeEvent
+    fun onTick(event: LorenzTickEvent) {
+        if (!isActive()) return
+        projectileData.forEach { (entity, data) -> data.update(entity) }
+    }
+
+    @SubscribeEvent
     fun onWorldRender(event: LorenzRenderWorldEvent) {
-        if (!LorenzUtils.inSkyBlock) return
+        if (!isActive()) return
         if (projectileData.isEmpty()) return
 
+        val drawLine = config.projectileLine
+
         RenderUtils.LineDrawer.draw3D(event.partialTicks) {
-            for ((entity, startPos) in projectileData) {
-                val motionVec = entity.getMotionLorenzVec()
-                val endPos = BlockUtils.rayTrace(startPos.pos, motionVec) ?: (startPos.pos + motionVec)
-
-                if (config.projectileLine) {
-                    draw3DLine(startPos.pos, endPos, Color.GREEN, 5, true)
-
+            for (data in projectileData.values) {
+                val startPos = data.startPos
+                val endPos = data.endPos ?: return@draw3D
+                if (drawLine) {
+                    draw3DLine(startPos, endPos, Color.GREEN, 5, true)
                 }
             }
+        }
+    }
+
+    override fun onDebug(builder: MutableList<String>) {
+        builder.add("Ghasts: $ghastCount")
+        builder.add("Projectiles: (${projectileData.size})")
+        for ((entity, data) in projectileData) {
+            builder.add("  - Pos: ${entity.getLorenzVec()}, Start: ${data.startPos}, End: ${data.endPos}")
         }
     }
 
