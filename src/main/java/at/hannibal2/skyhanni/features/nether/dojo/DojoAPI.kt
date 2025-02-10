18e9517f3551560482cc65a9fd41336bc67d020e
Empa <itsempa@users.noreply.github.com>
2025-02-10 10:51:47 +0100

    import
    Signed-off-by: Empa <itsempa@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/features/nether/dojo/DojoAPI.kt
+++ src/main/java/at/hannibal2/skyhanni/features/nether/dojo/DojoAPI.kt
@@ -12,7 +12,6 @@ import at.hannibal2.skyhanni.events.skyblock.ScoreboardAreaChangeEvent
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.utils.LocationUtils
 import at.hannibal2.skyhanni.utils.LocationUtils.contains
-import at.hannibal2.skyhanni.utils.LocationUtils.distanceToPlayer
 import at.hannibal2.skyhanni.utils.LorenzVec
 import at.hannibal2.skyhanni.utils.NumberUtil.formatInt
 import at.hannibal2.skyhanni.utils.RegexUtils.findMatcher
